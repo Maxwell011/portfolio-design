37a08b53b81c011e5839649e013615387a2cefc6
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-04 23:19:22 +0100

    Added  items

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -6,9 +6,11 @@ const Nav = () => {
       <section>
         {/* Social medial icon links */}
         <div>
-          <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl">
-            <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-[17.1429]">
-              <Linkedin />
+          <div>
+            <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl">
+              <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl ">
+                <Linkedin />
+              </div>
             </div>
           </div>
         </div>
