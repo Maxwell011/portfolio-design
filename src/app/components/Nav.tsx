00065132ae83b8b868f8b07faacb01056374cb04
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-04 20:18:12 +0100

    Added social icons to the nav

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,6 +1,31 @@
+import Linkedin from "../svg/Linkedin";
+
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
-export default Nav
+    <>
+      <section>
+        {/* Social medial icon links */}
+        <div>
+          <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl">
+            <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-[17.1429]">
+              <Linkedin />
+            </div>
+          </div>
+        </div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>Work</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+        <div>
+          <h2>ofomastanley007@gmail.com</h2>
+        </div>
+      </section>
+    </>
+  );
+};
+export default Nav;
