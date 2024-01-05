b62c95daa1150f6792d0aa17e3ea5a972773e2c7
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-05 20:53:34 +0100

    Added other social links

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,4 +1,6 @@
 import Linkedin from "../svg/Linkedin";
+import Twitter from "../svg/Twitter";
+import Dribble from "../svg/Dribble";
 
 const Nav = () => {
   return (
@@ -9,7 +11,7 @@ const Nav = () => {
           <div>
             <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
               <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
-                <Linkedin />
+                <Dribble />
               </div>
             </div>
             <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
@@ -19,7 +21,7 @@ const Nav = () => {
             </div>
             <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
               <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
-                <Linkedin />
+                <Twitter />
               </div>
             </div>
           </div>
