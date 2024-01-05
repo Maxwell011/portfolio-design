047c1031ddcc6addd2b203cd5def9e4f2cf6e601
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-05 21:39:30 +0100

    Refactor social media icon links in Nav component

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -8,7 +8,7 @@ const Nav = () => {
       <section>
         {/* Social medial icon links */}
         <div>
-          <div>
+          <div className="flex gap-[25px]">
             <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
               <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
                 <Dribble />
