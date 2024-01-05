349efa3147d20d22330eae618f2d7b7353823c59
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-05 20:38:21 +0100

    Refactor social media icons in Nav component

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -7,8 +7,8 @@ const Nav = () => {
         {/* Social medial icon links */}
         <div>
           <div>
-            <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl">
-              <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl ">
+            <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
+              <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
                 <Linkedin />
               </div>
             </div>
@@ -16,6 +16,7 @@ const Nav = () => {
         </div>
         <div>
           <ul>
+            {/* flex justify-center items-center gap-2.5 px-8 py-[5px] rounded-lg */}
             <li className="text-lg">Home</li>
             <li className="text-lg">Work</li>
             <li className="text-lg">About</li>
