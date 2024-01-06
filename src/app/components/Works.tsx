a160a984941be4d2fdec134550569b00755b918d
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-07 00:49:23 +0100

    Refactor Works component to include a styled header and description

--- src/app/components/Works.tsx
+++ src/app/components/Works.tsx
@@ -1,4 +1,17 @@
 const Works = () => {
-  return <div>Works</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <div className="px-4 py-3 border-y-4 border-y-white border-t-4 border-t-white border-solid">
+        <h1 className="uppercase text-[#fff] text-[40px] not-italic font-bold leading-[160%] ">
+          work
+        </h1>
+      </div>
+      <p className="text-white text-center text-lg font-medium leading-[160%] ">
+        Explore curated collections of projects that highlight my dedication to
+        creating user-centered experience. Each project is a testament to my
+        commitment to excellence and my ability to unique Approaches
+      </p>
+    </div>
+  );
 };
 export default Works;
