d2db07cd86a1da0c2923ae717d286ac2aa6ff363
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-06 22:15:09 +0100

    Update paragraph height and remove unnecessary spaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,10 @@ export default function Home() {
         </span>
         <br />A Product Designer.
       </h1>
-      <p className="w-[991px] h-[] text-center text-lg font-normal leading-[160%] ">
+      <p className="w-[991px] h-[87px] text-center text-lg font-normal leading-[160%] ">
         {" "}
-        I am a dedicated and enthusiastic product designer with a strong
-        passion for creating user-centered experiences. While designing to solve
+        I am a dedicated and enthusiastic product designer with a strong passion
+        for creating user-centered experiences. While designing to solve
         problems, I focus on the need to deliver effective conversion and align
         projects with business goals and objectives.
       </p>
