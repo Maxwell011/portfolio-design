57de45b176f6e05c876ab2fd402e47b9b9dfb4c7
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-07 00:56:38 +0100

    Update Works component styles

--- src/app/components/Works.tsx
+++ src/app/components/Works.tsx
@@ -1,12 +1,12 @@
 const Works = () => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center gap-5">
       <div className="px-4 py-3 border-y-4 border-y-white border-t-4 border-t-white border-solid">
         <h1 className="uppercase text-[#fff] text-[40px] not-italic font-bold leading-[160%] ">
           work
         </h1>
       </div>
-      <p className="text-white text-center text-lg font-medium leading-[160%] ">
+      <p className="text-white text-center text-lg font-medium leading-[160%] w-[954px]">
         Explore curated collections of projects that highlight my dedication to
         creating user-centered experience. Each project is a testament to my
         commitment to excellence and my ability to unique Approaches
