9cfbb4e233ab01b3525c9da302887a95ec7f5854
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-07 02:03:55 +0100

    Add constants file with initial work data

---
+++ constants/index.ts
@@ -0,0 +1,11 @@
+export const WORK_DATA = [
+  {
+    image: "/",
+    heading: "Landing Page Design",
+    title: "NFT Marketplace",
+    description:
+      "💎 Own a Piece of the Future: Imagine owning a digital masterpiece that's as unique as your fingerprint. With NFTs, you can acquire digital assets that are yours, and backed by blockchain technology. Discover the Future of Digital Ownership with Munmba! 🎉Step into a world where creativity meets technology, and art takes on a new form of expression. Welcome to Munmba, your gateway to the exciting universe of Non-Fungible Tokens (NFTs) – where every creation tells a unique story.",
+    tech: ["UI Design", "Prototyping", "NFT", "Collections", "Cryptocurrency"],
+    link: "",
+  },
+];
