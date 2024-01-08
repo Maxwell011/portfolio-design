d3146251eedc7804449df245378b3f367aa3cf20
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-08 16:35:18 +0100

    Add new landing page design for NFT Marketplace

--- constants/index.ts
+++ constants/index.ts
@@ -8,4 +8,13 @@ export const WORK_DATA = [
     tech: ["UI Design", "Prototyping", "NFT", "Collections", "Cryptocurrency"],
     link: "",
   },
+  {
+    image: "/Rectangle 23 (1).png",
+    heading: "Landing Page Design",
+    title: "NFT Marketplace",
+    description:
+      "💎 Own a Piece of the Future: Imagine owning a digital masterpiece that's as unique as your fingerprint. With NFTs, you can acquire digital assets th...",
+    tech: ["UI Design", "Prototyping", "NFT", "Collections", "Cryptocurrency"],
+    link: "",
+  },
 ];
