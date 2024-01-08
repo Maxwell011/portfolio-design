6e21ee3d0eb3eb2e037e66cef33c05d4e01c46d7
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-08 16:39:44 +0100

    Update user flow and title in constants/index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -10,11 +10,11 @@ export const WORK_DATA = [
   },
   {
     image: "/Rectangle 23 (1).png",
-    heading: "Landing Page Design",
-    title: "NFT Marketplace",
+    heading: "User Flow",
+    title: "QuickShop",
     description:
-      "💎 Own a Piece of the Future: Imagine owning a digital masterpiece that's as unique as your fingerprint. With NFTs, you can acquire digital assets th...",
-    tech: ["UI Design", "Prototyping", "NFT", "Collections", "Cryptocurrency"],
+      "Welcome to our one-stop online store for all your football needs! We offer a wide range of high-quality football equipment, including jerseys, shin pad...",
+    tech: ["Mobile Design", "Website Design", "Responsive Design", "Figma"],
     link: "",
   },
 ];
