cdc5de5bae3814487be939a9bdd6060320c1d05e
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-09 00:51:59 +0100

    Add Framer component

---
+++ src/app/svg/Framer.tsx
@@ -0,0 +1,25 @@
+export default function Framer() {
+  return (
+    <section>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="40"
+        height="60"
+        viewBox="0 0 40 60"
+        fill="none"
+      >
+        <g clip-path="url(#clip0_75_19750)">
+          <path
+            d="M0 0H40V20H20L0 0ZM0 20H20L40 40H20V60L0 40V20Z"
+            fill="white"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_75_19750">
+            <rect width="40" height="60" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    </section>
+  );
+}
