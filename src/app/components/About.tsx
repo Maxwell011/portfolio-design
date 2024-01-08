067d2c66b7fac24000bf69a604599f9f6483ff72
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-09 00:32:27 +0100

    Refactor About component to update styling

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,11 +1,15 @@
 const About = () => {
-    return (
-        <>
-        <section>
-            <h1>About me</h1>
-        </section>
-        </>
-    )
-}
+  return (
+    <>
+      <section>
+        <div className="px-4 py-3 border-y-4 border-y-white border-t-4 border-t-white border-solid">
+          <h1 className="text-white text-[40px] font-bold leading-[160%]">
+            ABOUT ME
+          </h1>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default About
+export default About;
