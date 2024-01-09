f4d81c0fdea422035d326b8c58245aa42c8f2a1c
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-09 10:46:20 +0100

    Fix typo in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         </h1>
         <p className="w-[935px] h-[87px] text-center text-lg font-normal leading-[160%] text-[#fff] font-plex mb-10">
           {" "}
-          I am a dedicated and enthusiastic product designer with a strong
+          I&quot;m a dedicated and enthusiastic product designer with a strong
           passion for creating user-centered experiences. While designing to
           solve problems, I focus on the need to deliver effective conversion
           and align projects with business goals and objectives.
