66d086f62b38e6f438750ff3a1ee69970ca1b935
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-09 10:45:04 +0100

    Update Contact component header

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -2,8 +2,11 @@ const Contact = () => {
   return (
     <>
       <section>
-        <h1>Contact Me</h1>
-      </section>
+      <div className="px-4 py-3 border-y-4 border-y-white border-t-4 border-t-white border-solid">
+          <h1 className="text-white text-[40px] font-bold leading-[160%]">
+            CONTACT ME
+          </h1>
+        </div>      </section>
     </>
   );
 };
