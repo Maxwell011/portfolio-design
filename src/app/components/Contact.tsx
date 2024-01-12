2ace6bf7f54cd09e715869d52b40fcd38a4ca71d
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-12 13:38:17 +0100

    Update Contact component with additional content

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -2,11 +2,27 @@ const Contact = () => {
   return (
     <>
       <section>
-      <div className="px-4 py-3 border-y-4 border-y-white border-t-4 border-t-white border-solid">
+        <div className="px-4 py-3 border-y-4 border-y-white border-t-4 border-t-white border-solid">
           <h1 className="text-white text-[40px] font-bold leading-[160%]">
             CONTACT ME
           </h1>
-        </div>      </section>
+        </div>{" "}
+        <p className="text-white text-lg font-medium">
+          Let&apos s connect and discuss how I can be an asset to your team.
+          I&apos m here and ready to contribute.
+        </p>
+        <h2>🌟Email me at:</h2>
+        <div className="h-[52px] w-[327px] px-6 py-2.5 rounded-lg border-2 border-solid border-[#CDAF75]">
+          <h2 className=" text-xl font-normal leading-[normal] tracking-[0.4px] underline text-[#fff]">
+            ofomastanley007@gmail.com
+          </h2>
+        </div>
+        <p>OR</p>
+        <h2>Send a message</h2>
+        <label>Email</label>
+        <input type="text" name="" id="" />
+        <label>Message</label>
+      </section>
     </>
   );
 };
