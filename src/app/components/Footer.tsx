d74ec67b36c09757bab8b4bb642cfa686931c59f
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-12 14:37:48 +0100

    Add social media icons to Footer component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,28 @@
+import Linkedin from "../svg/Linkedin";
+import Twitter from "../svg/Twitter";
+import Dribble from "../svg/Dribble";
+
 export default function Footer() {
     return (
         <>
         <section>
-            <h1>Footer</h1>
-        </section>
+        <div className="flex gap-[25px]">
+            <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
+              <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
+                <Dribble />
+              </div>
+            </div>
+            <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
+              <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
+                <Linkedin />
+              </div>
+            </div>
+            <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
+              <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
+                <Twitter />
+              </div>
+            </div>
+          </div>        </section>
         </>
     )
 }
