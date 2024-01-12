cb125f011de30f0310cc3cadfae9e86dfb18e664
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-12 13:38:02 +0100

    Add flex layout to About section

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -10,7 +10,7 @@ import Adobexd from "../svg/Adobexd";
 const About = () => {
   return (
     <>
-      <section>
+      <section className="flex flex-col items-center">
         <div className="px-4 py-3 border-y-4 border-y-white border-t-4 border-t-white border-solid">
           <h1 className="text-white text-[40px] font-bold leading-[160%]">
             ABOUT ME
