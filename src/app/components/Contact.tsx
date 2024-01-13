52e41eab5371a74aab76d95c679b07069cd38bdb
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-13 22:05:34 +0100

    Refactor Contact component UI

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -11,15 +11,21 @@ const Contact = () => {
           Let&apos s connect and discuss how I can be an asset to your team.
           I&apos m here and ready to contribute.
         </p>
-        <h2>🌟Email me at:</h2>
+        <h2 className="text-white text-2xl font-medium leading-[160%]">
+          🌟Email me at:
+        </h2>
         <div className="h-[52px] w-[327px] px-6 py-2.5 rounded-lg border-2 border-solid border-[#CDAF75]">
           <h2 className=" text-xl font-normal leading-[normal] tracking-[0.4px] underline text-[#fff]">
             ofomastanley007@gmail.com
           </h2>
         </div>
-        <p>OR</p>
-        <h2>Send a message</h2>
-        <label>Email</label>
+        <p className="text-white text-2xl font-medium leading-[160%]">OR</p>
+        <h2 className="text-white text-2xl font-medium leading-[160%]">
+          Send a message
+        </h2>
+        <label className="text-white text-lg not-italic font-normal leading-[160%]">
+          Email
+        </label>
         <input type="text" name="" id="" />
         <label>Message</label>
       </section>
