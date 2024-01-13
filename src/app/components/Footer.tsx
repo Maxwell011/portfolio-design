ba49a0f82eec718abacdf8d0979fb191317d1d42
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-13 22:07:04 +0100

    Refactor Footer component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,26 +3,35 @@ import Twitter from "../svg/Twitter";
 import Dribble from "../svg/Dribble";
 
 export default function Footer() {
-    return (
-        <>
-        <section>
+  return (
+    <>
+      <section className="h-[204px] bg-black">
         <div className="flex gap-[25px]">
-            <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
-              <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
-                <Dribble />
-              </div>
+          <h2 className="text-white text-2xl font-medium leading-[160%]">
+            Socials:
+          </h2>
+          <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
+            <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
+              <Dribble />
             </div>
-            <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
-              <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
-                <Linkedin />
-              </div>
+          </div>
+          <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
+            <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
+              <Linkedin />
             </div>
-            <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
-              <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
-                <Twitter />
-              </div>
+          </div>
+          <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
+            <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
+              <Twitter />
             </div>
-          </div>        </section>
-        </>
-    )
+          </div>
+          <div>
+            <p className="text-white text-lg font-normal">
+              ©2023 Stanley-Asokingz. All Rights Reserved
+            </p>
+          </div>
+        </div>{" "}
+      </section>
+    </>
+  );
 }
