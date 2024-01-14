c622f13a46d235e0ea9c2da10111c95134cc1328
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-14 23:21:00 +0100

    Refactor Contact component markup

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -16,9 +16,9 @@ const Contact = () => {
           🌟Email me at:
         </h2>
         <div className="h-[52px] w-[327px] px-6 py-2.5 rounded-lg border-2 border-solid border-[#CDAF75]">
-          <h2 className=" text-xl font-normal leading-[normal] tracking-[0.4px] underline text-[#fff]">
+          <button className=" text-xl font-normal leading-[normal] tracking-[0.4px] underline text-[#fff]">
             ofomastanley007@gmail.com
-          </h2>
+          </button>
         </div>
         <p className="text-white text-2xl font-medium leading-[160%] my-10">
           OR
@@ -26,23 +26,27 @@ const Contact = () => {
         <h2 className="text-white text-2xl font-medium leading-[160%]">
           Send a message
         </h2>
-        <label className="text-white text-lg not-italic font-normal leading-[160%] mt-6">
-          Email
-        </label>
-        <input
-          type="text"
-          name=""
-          id=""
-          className="border flex w-[553px] h-[52px] items-center gap-2.5 px-4 py-2.5 rounded-lg bg-[#183135] border-solid border-[#9E9E9E]"
-          placeholder="Your email address"
-        />
-        <label className="text-white text-lg font-normal leading-[160%] mt-5">
-          Message
-        </label>
-        <textarea
-          className="flex w-[553px] h-40 items-start gap-2.5 px-4 py-[15.5px] rounded-lg border-solid border-[#9E9E9E] bg-[#183135]"
-          placeholder="Type your message here"
-        ></textarea>
+        <div>
+          <label className="text-white text-lg not-italic font-normal leading-[160%] mt-6">
+            Email
+          </label>
+          <input
+            type="text"
+            name=""
+            id=""
+            className="border flex w-[553px] h-[52px] items-center gap-2.5 px-4 py-2.5 rounded-lg bg-[#183135] border-solid border-[#9E9E9E]"
+            placeholder="Your email address"
+          />
+        </div>
+        <div className="mt-5">
+          <label className="text-white text-lg font-normal leading-[160%] ">
+            Message
+          </label>
+          <textarea
+            className="flex w-[553px] h-40 items-start gap-2.5 px-4 py-[15.5px] rounded-lg border-solid border-[#9E9E9E] bg-[#183135]"
+            placeholder="Type your message here"
+          ></textarea>
+        </div>
         <div className="flex justify-center items-center gap-2 px-[23.5px] py-[14.5px] rounded-lg bg-gold w-[553px] h-[52px] mt-8">
           <button className="text-black text-lg font-semibold tracking-[0.36px] uppercase ">
             Send Message
