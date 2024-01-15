f74ad14e966096c8297cdbeae7784ec20eb7c157
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-15 15:24:10 +0100

    Update Works component class name and add id attribute to section element

--- src/app/components/Works.tsx
+++ src/app/components/Works.tsx
@@ -6,7 +6,7 @@ import CrimsonEllipse from "../svg/CrimsonEllipse";
 
 const Works = () => {
   return (
-    <section className="">
+    <section className="" id="work">
       <div className="flex flex-col items-center gap-5">
         <div className="px-4 py-3 border-y-4 border-y-white border-t-4 border-t-white border-solid">
           <h1 className="uppercase text-[#fff] text-[40px] not-italic font-bold leading-[160%] ">
