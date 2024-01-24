6da9b7c033909919c1c0b9388d4bd947b10bc85e
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-24 16:46:47 +0100

    Update Vitals page content

--- src/app/Vitals/page.tsx
+++ src/app/Vitals/page.tsx
@@ -1,7 +1,37 @@
 export default function Vitals() {
   return (
     <section>
-      <h1>This is the vitals</h1>
+      <h1>Vitals - Emr Software</h1>
+      <p>
+        Vitals is an electronic medical record software with integrated
+        blockchain technology which aims to revolutionize the management and
+        security of medical records. Vitals offers you the best decentralized
+        EMR system that gives full control to your health records and help you
+        keep them safe and immutable.
+      </p>
+      <p>
+        Overall, Vitals offers the potential to improve data security, patient
+        privacy, and interoperability in healthcare while empowering patients to
+        have greater control over their medical records.
+      </p>
+      {/* Image Cover */}
+
+      <div>
+        <div>
+          <h2>Task</h2>
+          <p>
+            Create an Electronic Medical Record software that challenges the
+            manual method of documenting patient’s health record.
+          </p>
+        </div>
+        <div>
+          <h2>Result</h2>
+          <p></p>
+        </div>
+        <div>
+          <h2>Team</h2>
+        </div>
+      </div>
     </section>
   );
 }
