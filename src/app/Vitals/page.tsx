6adf7236a8c685dfa1573672eceb7d6ac0556fef
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-24 16:57:04 +0100

    Added team information, duration, role, problem statement, solutions, research, and building empathy sections

--- src/app/Vitals/page.tsx
+++ src/app/Vitals/page.tsx
@@ -30,8 +30,55 @@ export default function Vitals() {
         </div>
         <div>
           <h2>Team</h2>
+          {/* team images */}
+        </div>
+        <div>
+          <h2>Duration</h2>
+          <p>8 Weeks</p>
+          <h2>Role</h2>
+          <p>UI/UX Designer, Design Lead, Sprint Member</p>
         </div>
       </div>
+
+      <h2>Problem Statement</h2>
+      <p>
+        The healthcare industry faces challenges in managing EMRs due to issues
+        like data fragmentation, limited interoperability, privacy concerns, and
+        vulnerabilities to unauthorized access. Centralized EMR systems are
+        prone to data breaches and system failures, hindering collaboration and
+        compromising patient privacy.
+      </p>
+      <h2>Our Solutions</h2>
+      <p>
+        Our solution leverages blockchain technology to address key challenges
+        in EMR management, ensuring data integrity and security, promoting
+        interoperability and data sharing, protecting patient privacy and
+        consent, and streamlining healthcare processes.
+      </p>
+
+      {/* Image */}
+      <h2>Research</h2>
+      <p>
+        Since there was an already existing ecosystem for EMR software, my team
+        and I conducted in-depth interviews with healthcare professionals,
+        including doctors, nurses, and administrative staff, to understand their
+        daily workflows and pain points when using EMR software. We also met
+        various individuals and asked them about their experience with their
+        medical record, privacy and possible changes they would want to have.
+        <span>Through the research, we intend to:</span>
+        Identify the target market for EMR software Understand users
+        frustrations Define a problem statement
+      </p>
+
+      <h2>Building Empathy</h2>
+      <p>
+        Empathizing with users ensure that my team and I understands users need
+        and it will help us to create products and experiences that truly
+        resonate with users. This empathetic approach ensures that design
+        solutions are not only functional but also meaningful and enjoyable for
+        the people who use them. Ultimately, empathy is the foundation for
+        creating user experiences that leave a lasting positive impression.
+      </p>
     </section>
   );
 }
