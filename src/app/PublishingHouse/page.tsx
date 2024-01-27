52f64f885cae97e089078373e73dc555ce4af568
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-27 22:41:21 +0100

    Add content to PublishingHouse page

--- src/app/PublishingHouse/page.tsx
+++ src/app/PublishingHouse/page.tsx
@@ -1,3 +1,45 @@
 export default function PublishingHouse() {
-  return <section></section>;
+  return (
+    <section>
+      <h1>SAFAR-BOOKIES</h1>
+      <p>
+        🌍 Safars Beyond Borders SafarBookies transcends boundaries, bringing
+        you stories from around the globe. Immerse yourself in cultures,
+        experiences, and perspectives through our diverse selection of
+        international literature.
+      </p>
+      {/* image */}
+      <div>
+        <div>
+          <h2>Task</h2>
+          <p>
+            Design a landing page that will aid and contribute to the overall
+            success of marketing campaigns and user interactions of a publishing
+            house.
+          </p>
+        </div>
+        <div>
+          <h2>Result</h2>
+          <p>
+            Designed a cohesive and consistent experience that reflects the
+            brand identity, values, and messaging. A visually appealing and
+            effective landing page that resonates with the target audience and
+            reinforces the brand presence.
+          </p>
+        </div>
+        <div>
+          <h2>Duration</h2>
+          <p>6 Hours</p>
+          <h2>Role</h2>
+          <p>UI/UX Designer</p>
+        </div>
+      </div>
+      <h2></h2>
+      {/* image */}
+      <h2>Full landing page design</h2>
+      {/* image */}
+      <h3>Check out</h3>
+      <h3>other projects</h3>{" "}
+    </section>
+  );
 }
