544817193f5e8796f7e7c2fe724577a8b9632c9e
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-27 22:27:55 +0100

    Update MarketPlace page content

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,7 +1,45 @@
 export default function MarketPlace() {
   return (
     <section>
-      <h1>Brother Sun</h1>
+      <h1>Munmba</h1>
+      <p>
+        💎 Own a Piece of the Future: Imagine owning a digital masterpiece that
+        as unique as your fingerprint. With NFTs, you can acquire digital assets
+        that are yours, and backed by blockchain technology. Discover the Future
+        of Digital Ownership with Munmba! 🎉 Step into a world where creativity
+        meets technology, and art takes on a new form of expression. Welcome to
+        Munmba, your gateway to the exciting universe of Non-Fungible Tokens
+        (NFTs) – where every creation tells a unique story.
+      </p>
+      <div>
+        <div>
+          <h2>Task</h2>
+          <p>
+            Create a user flow for users to access an ecommerce website and view
+            product details. Make it mobile responsive.
+          </p>
+        </div>
+        <div>
+          <h2>Result</h2>
+          <p>
+            Designed both screens for mobile and desktop to ensure ease of use
+            and consistent user experience for users as well as wider audience
+            reach and improved conversion rate for the business.{" "}
+          </p>
+        </div>
+        <div>
+          <h2>Duration</h2>
+          <p>2 Days</p>
+          <h2>Role</h2>
+          <p>UI/UX Designer</p>
+        </div>
+      </div>
+      <h2>Desktop Screens</h2>
+      {/* image */}
+      <h2>Mobile Screens</h2>
+      {/* image */}
+      <h3>Check out</h3>
+      <h3>other projects</h3>{" "}
     </section>
   );
 }
