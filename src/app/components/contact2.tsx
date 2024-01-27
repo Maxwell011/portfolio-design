89be02b0a1a3ad4e92650ffc16f09fe921326956
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-28 00:27:55 +0100

    Add contact information and social media icons to contact2 component

--- src/app/components/contact2.tsx
+++ src/app/components/contact2.tsx
@@ -1,5 +1,41 @@
+import Linkedin from "../svg/Linkedin";
+import Twitter from "../svg/Twitter";
+import Dribble from "../svg/Dribble";
+import Link from "next/link";
+
 export default function contact2() {
-  return <div>
-    <
-  </div>;
+  return (
+    <div>
+      <p>
+        Thank you for your time! 🙌 Your interest means a lot to me, and I am
+        excited about the possibilities ahead. Looking forward to the next steps{" "}
+      </p>
+      <h1>Let stay connected for future opportunities! 🌟 </h1>
+      <div className="h-[52px] w-[327px] px-6 py-2.5 rounded-lg border-2 border-solid border-[#CDAF75]">
+        <Link
+          href=""
+          className=" text-xl font-normal leading-[normal] tracking-[0.4px] underline text-[#fff]"
+        >
+          ofomastanley007@gmail.com
+        </Link>
+      </div>
+      <div className="flex gap-[25px]">
+        <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
+          <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
+            <Dribble />
+          </div>
+        </div>
+        <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
+          <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
+            <Linkedin />
+          </div>
+        </div>
+        <div className="w-[34.286px] h-[34.286px] opacity-[0.58] bg-base rounded-2xl flex items-center justify-center ">
+          <div className="w-12 h-12 opacity-[0.29] bg-base rounded-3xl flex items-center justify-center p-5">
+            <Twitter />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
