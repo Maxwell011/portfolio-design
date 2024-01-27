b9c5bce453f0dff507ae1aa2fba910d9bdac4500
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-27 22:15:22 +0100

    Update Quickshop page content

--- src/app/Quickshop/page.tsx
+++ src/app/Quickshop/page.tsx
@@ -1,7 +1,36 @@
 export default function QuickShop() {
   return (
     <section>
-      <h1>Night School</h1>
+      <h1>Quickshop</h1>
+      <p>
+        Welcome to our one-stop online store for all your football needs! We
+        offer a wide range of high-quality football equipment, including
+        jerseys, shin pads, footballs, boots, goalkeeper gloves, and training
+        equipment. At our store, we passionate about football, and we believe
+        that everyone should have access to high-quality equipment and training
+        gear. That why we only stock products from trusted brands and offer them
+        at competitive prices. Shop with us today and get ready to dominate the
+        field!
+      </p>
+      <div>
+        <div>
+          <h2>Task</h2>
+          <p>
+            Create an Electronic Medical Record software that challenges the
+            manual method of documenting patient’s health record.
+          </p>
+        </div>
+        <div>
+          <h2>Result</h2>
+          <p></p>
+        </div>
+        <div>
+          <h2>Duration</h2>
+          <p>8 Weeks</p>
+          <h2>Role</h2>
+          <p>UI/UX Designer, Design Lead, Sprint Member</p>
+        </div>
+      </div>
     </section>
   );
 }
