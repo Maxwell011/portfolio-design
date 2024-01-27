42fff694bac50d29b46ee00b3d106f5397169be5
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-01-27 22:53:38 +0100

    Add mobile responsiveness for Meet D Expert page

--- src/app/Expert/page.tsx
+++ src/app/Expert/page.tsx
@@ -1,3 +1,49 @@
 export default function MeetExpert() {
-  return <section></section>;
+  return (
+    <section>
+      {" "}
+      <h1>Meet D Experts</h1>
+      <p>
+        We exist to help people get answers to questions they’ve not found.
+        Either that they cannot ask them or they do not know to frame them, or
+        the answers are not framed well. Either that they cannot ask them or
+        they do not know to frame them, or the answers are not framed well.
+      </p>
+      {/* image */}
+      <div>
+        <div>
+          <h2>Task</h2>
+          <p>
+            With the existing desktop screen for the product “Meet D Expert,
+            design a mobile responsiveness for users to seamlessly access and
+            engage with our product on their smartphones, whether you on the go
+            or relaxing at home.
+          </p>
+        </div>
+        <div>
+          <h2>Result</h2>
+          <ul>
+            <li>📈 Boosted Accessibility</li>
+            <li>🌐 Cross-Device Consistency</li>
+            <li>🔒 Security on Every Screen</li>
+            <li>🌍 Stay Connected, Anywhere</li>
+            <li>💡 Intuitive Interface</li>
+            <li>📱 Unmatched User Experience</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Duration</h2>
+          <p>6 Days</p>
+          <h2>Role</h2>
+          <p>Moblie Responsive Designer</p>
+        </div>
+      </div>
+      <h2>Overview</h2>
+      {/* image */}
+      <h2>Full landing page design</h2>
+      {/* image */}
+      <h3>Check out</h3>
+      <h3>other projects</h3>{" "}
+    </section>
+  );
 }
