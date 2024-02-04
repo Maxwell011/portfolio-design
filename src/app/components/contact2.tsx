4762c304305bf6c8f0fe1bba7d9c07844f404009
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-02-04 22:01:06 +0100

    Update contact2 component styling

--- src/app/components/contact2.tsx
+++ src/app/components/contact2.tsx
@@ -5,15 +5,15 @@ import Link from "next/link";
 
 export default function contact2() {
   return (
-    <div>
-      <p className="text-lg font-normal text-white">
+    <div className="bg-abyss flex flex-col items-center p-[100px]">
+      <p className="text-lg font-normal text-white mb-10">
         Thank you for your time! 🙌 Your interest means a lot to me, and I am
         excited about the possibilities ahead. Looking forward to the next steps{" "}
       </p>
-      <h1 className="text-[32px] font-medium leading-[51px]">
+      <h1 className="text-[32px] font-medium leading-[51px] text-white">
         Let stay connected for future opportunities! 🌟{" "}
       </h1>
-      <div className="h-[52px] w-[327px] px-6 py-2.5 rounded-lg border-2 border-solid border-[#CDAF75]">
+      <div className="h-[52px] w-[327px] px-6 py-2.5 rounded-lg border-2 border-solid border-[#b4a78b] mt-5 mb-[30px]">
         <Link
           href=""
           className=" text-xl font-normal leading-[normal] tracking-[0.4px] underline text-[#fff]"
