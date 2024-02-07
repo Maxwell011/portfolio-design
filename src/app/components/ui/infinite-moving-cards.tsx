b39a5448cb24084745ac4cbd3fba3530846c5028
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-02-07 23:19:39 +0100

    Remove unnecessary div element in InfiniteMovingCards component

--- src/app/components/ui/infinite-moving-cards.tsx
+++ src/app/components/ui/infinite-moving-cards.tsx
@@ -100,7 +100,6 @@ export const InfiniteMovingCards = ({
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
                   {item.name}
