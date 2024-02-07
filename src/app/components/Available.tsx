978772253a4eb007f64b159f2581799bc9275cf1
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-02-07 22:37:28 +0100

    Update testimonials to projects in InfiniteMovingCardsDemo

--- src/app/components/Available.tsx
+++ src/app/components/Available.tsx
@@ -6,17 +6,13 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 export default function InfiniteMovingCardsDemo() {
   return (
     <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-      <InfiniteMovingCards
-        items={testimonials}
-        direction="right"
-        speed="slow"
-      />
-      <InfiniteMovingCards items={testimonials} direction="left" speed="slow" />
+      <InfiniteMovingCards items={projects} direction="right" speed="slow" />
+      <InfiniteMovingCards items={projects} direction="left" speed="slow" />
     </div>
   );
 }
 
-const testimonials = [
+const projects = [
   {
     quote:
       "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
