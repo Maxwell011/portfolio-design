2102001afb7e98211903ed331a82018f0edfadcd
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-02-08 10:25:30 +0100

    Refactor InfiniteMovingCardsDemo component

--- src/app/components/Available.tsx
+++ src/app/components/Available.tsx
@@ -6,12 +6,18 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 export default function InfiniteMovingCardsDemo() {
   return (
     <div className="h-[40rem] rounded-md flex flex-col antialiased dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-      <InfiniteMovingCards
-        items={testimonials}
-        direction="right"
-        speed="slow"
-      />
-      <InfiniteMovingCards items={testimonials} direction="left" speed="slow" />
+      <div className="flex flex-col gap-2">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="left"
+          speed="slow"
+        />
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+      </div>
     </div>
   );
 }
