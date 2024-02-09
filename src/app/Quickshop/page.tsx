788604c16e705d44ec306b4b627975b07e0dced0
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-02-09 21:52:16 +0100

    Styled heading

--- src/app/Quickshop/page.tsx
+++ src/app/Quickshop/page.tsx
@@ -1,46 +1,48 @@
 export default function QuickShop() {
   return (
     <section>
-      <h1>Quickshop</h1>
-      <p>
-        Welcome to our one-stop online store for all your football needs! We
-        offer a wide range of high-quality football equipment, including
-        jerseys, shin pads, footballs, boots, goalkeeper gloves, and training
-        equipment. At our store, we passionate about football, and we believe
-        that everyone should have access to high-quality equipment and training
-        gear. That why we only stock products from trusted brands and offer them
-        at competitive prices. Shop with us today and get ready to dominate the
-        field!
-      </p>
-      <div>
+      <section className="bg-black flex flex-col items-center justify-center text-center px-[100px] py-0">
+        <h1 className="text-[64px] font-bold uppercase">Quickshop</h1>
+        <p className="text-white text-lg font-normal tracking-[0.36px]">
+          Welcome to our one-stop online store for all your football needs! We
+          offer a wide range of high-quality football equipment, including
+          jerseys, shin pads, footballs, boots, goalkeeper gloves, and training
+          equipment. At our store, we passionate about football, and we believe
+          that everyone should have access to high-quality equipment and
+          training gear. That why we only stock products from trusted brands and
+          offer them at competitive prices. Shop with us today and get ready to
+          dominate the field!
+        </p>
         <div>
-          <h2>Task</h2>
-          <p>
-            Create a user flow for users to access an ecommerce website and view
-            product details. Make it mobile responsive.
-          </p>
+          <div>
+            <h2>Task</h2>
+            <p>
+              Create a user flow for users to access an ecommerce website and
+              view product details. Make it mobile responsive.
+            </p>
+          </div>
+          <div>
+            <h2>Result</h2>
+            <p>
+              Designed both screens for mobile and desktop to ensure ease of use
+              and consistent user experience for users as well as wider audience
+              reach and improved conversion rate for the business.{" "}
+            </p>
+          </div>
+          <div>
+            <h2>Duration</h2>
+            <p>2 Days</p>
+            <h2>Role</h2>
+            <p>UI/UX Designer</p>
+          </div>
         </div>
-        <div>
-          <h2>Result</h2>
-          <p>
-            Designed both screens for mobile and desktop to ensure ease of use
-            and consistent user experience for users as well as wider audience
-            reach and improved conversion rate for the business.{" "}
-          </p>
-        </div>
-        <div>
-          <h2>Duration</h2>
-          <p>2 Days</p>
-          <h2>Role</h2>
-          <p>UI/UX Designer</p>
-        </div>
-      </div>
-      <h2>Desktop Screens</h2>
-      {/* image */}
-      <h2>Mobile Screens</h2>
-      {/* image */}
-      <h3>Check out</h3>
-      <h3>other projects</h3>
+        <h2>Desktop Screens</h2>
+        {/* image */}
+        <h2>Mobile Screens</h2>
+        {/* image */}
+        <h3>Check out</h3>
+        <h3>other projects</h3>
+      </section>
     </section>
   );
 }
