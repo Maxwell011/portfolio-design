9211d9b16db58b3834eab1e673f5ab4cfad582ca
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-02-26 23:10:57 +0100

    Added cover image and chain component

--- src/app/Autofixhub/page.tsx
+++ src/app/Autofixhub/page.tsx
@@ -5,6 +5,20 @@ export default function AutoFixHub() {
   return (
     <section>
       <h1 className="text-[64px] font-bold uppercase">REVAMPING Autofixhub</h1>
+      <div className="flex w-[1210px] h-[845px] justify-center items-center rounded-[40px] bg-[#233038]">
+        {" "}
+        <Image
+          src="/Cover page Expert.png"
+          width={1240}
+          height={779}
+          alt="work image"
+          className=""
+        />
+      </div>
+      <div className="flex gap-[50rem]">
+        <Chain />
+        <Chain />
+      </div>
     </section>
   );
 }
